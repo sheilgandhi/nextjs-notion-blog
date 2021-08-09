0046bee05e4f04281f54e47f92fd0eaa0bcfb3b5
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2021-08-09 16:59:47 +1200

    Animation improved

--- components/Header.js
+++ components/Header.js
@@ -19,23 +19,23 @@ function Header() {
                 <p className="px-5">Link4</p>
             </div>
             {/* Tablet */}
-            <div className="inline-flex md:hidden" onClick={() => setVisible(!visible)}>
+            <div className="inline-flex md:hidden">
                 {
                     visible ?
                     <div className="fixed top-0 right-0 bottom-0 z-10 bg-white w-72 h-screen"> {/* X */}
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 absolute top-3 right-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer absolute top-3 right-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" onClick={() => setVisible(!visible)}>
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                         </svg>
-                        <div className="font-medium text-lg pt-10">
-                        <p className="px-5 py-2 border-b-2 border-gray-500">Link1</p>
-                        <p className="px-5 py-2 border-b-2 border-gray-500">Link2</p>
-                        <p className="px-5 py-2 border-b-2 border-gray-500">Link3</p>
-                        <p className="px-5 py-2 border-b-2 border-gray-500">Link4</p>
+                        <div className="font-medium text-lg mt-10 cursor-pointer">
+                        <p className="mx-5 py-2 border-b-2 border-gray-200">Link1</p>
+                        <p className="mx-5 py-2 border-b-2 border-gray-200">Link2</p>
+                        <p className="mx-5 py-2 border-b-2 border-gray-200">Link3</p>
+                        <p className="mx-5 py-2 border-b-2 border-gray-200">Link4</p>
                         </div>
                     </div>  
                     :
                     <div> {/* Hamburger Menu */}
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer absolute top-3 right-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" onClick={() => setVisible(!visible)}>
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                         </svg>
                     </div>
